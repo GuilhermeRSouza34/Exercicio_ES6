22905500ea9cc419ced97fcb4b6270a71fdf8e51
Guilherme <guilhermersouza34@gmail.com>
2024-04-17 15:47:46 -0300

    Fazendo a Leitura e Vendo as medias

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { alunosAprovados } from './alunos.js';
+import fs from 'fs';
+
+// Lendo os dados do arquivo JSON
+fs.readFile('alunos.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Erro ao ler o arquivo de alunos:', err);
+        return;
+    }
+
+    const alunos = JSON.parse(data);
+
+    // Filtrando os alunos aprovados
+    const alunosAprovadosArray = alunosAprovados(alunos);
+
+    console.log('Alunos aprovados:');
+    console.log(alunosAprovadosArray);
+});
